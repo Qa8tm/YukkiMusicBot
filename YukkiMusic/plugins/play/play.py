fc032a78079389dbb52feece955e72ce805989ad
UUCCTT <98736460+UUCCTT@users.noreply.github.com>
2022-03-17 20:16:20 +0200

    Update play.py

--- YukkiMusic/plugins/play/play.py
+++ YukkiMusic/plugins/play/play.py
@@ -22,7 +22,7 @@ from strings import get_command
 from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram,
                         YouTube, app)
 from YukkiMusic.core.call import Yukki
-from Yukkimusic.plugins.filters import command
+from YukkiMusic.plugins.filters import command
 from YukkiMusic.utils import seconds_to_min, time_to_seconds
 from YukkiMusic.utils.channelplay import get_channeplayCB
 from YukkiMusic.utils.database import is_video_allowed
