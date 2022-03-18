f4f9beb4eb051f308a27131707aa64c4ccd28e22
UUCCTT <98736460+UUCCTT@users.noreply.github.com>
2022-03-19 01:58:18 +0200

    Update inlinequery.py

--- YukkiMusic/utils/inlinequery.py
+++ YukkiMusic/utils/inlinequery.py
@@ -15,6 +15,12 @@ answer = []
 answer.extend(
     [
         InlineQueryResultArticle(
+            title="End Stream",
+            description="Stop the ongoing playout on group call.",
+            thumb_url="https://telegra.ph/file/d2eb03211baaba8838cc4.png",
+            input_message_content=InputTextMessageContent("/stop"),
+        ),
+        InlineQueryResultArticle(
             title="Pause Stream",
             description=f"Pause the current playout on group call.",
             thumb_url="https://telegra.ph/file/c0a1c789def7b93f13745.png",
