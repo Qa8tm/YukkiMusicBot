8aefe2e2ce6cd5861a54b7331fda1d8f22afd863
UUCCTT <98736460+UUCCTT@users.noreply.github.com>
2022-03-18 03:10:09 +0200

    Update play.py

--- YukkiMusic/plugins/play/play.py
+++ YukkiMusic/plugins/play/play.py
@@ -41,7 +41,7 @@ PLAY_COMMAND = get_command("PLAY_COMMAND")
 
 
 @app.on_message(
-    command([PLAY_COMMAND, "test"])
+    command(PLAY_COMMAND)
     & filters.group
     & ~filters.edited
     & ~BANNED_USERS
