03e11441415832f84fd612132ba38adf50d33398
UUCCTT <98736460+UUCCTT@users.noreply.github.com>
2022-03-18 02:02:15 +0200

    Update cleanmode.py

--- YukkiMusic/plugins/misc/cleanmode.py
+++ YukkiMusic/plugins/misc/cleanmode.py
@@ -38,34 +38,6 @@ IS_BROADCASTING = False
 cleanmode_group = 15
 
 
-@app.on_raw_update(group=cleanmode_group)
-async def clean_mode(client, update, users, chats):
-    global IS_BROADCASTING
-    if IS_BROADCASTING:
-        return
-    try:
-        if not isinstance(update, types.UpdateReadChannelOutbox):
-            return
-    except:
-        return
-    if users:
-        return
-    if chats:
-        return
-    message_id = update.max_id
-    chat_id = int(f"-100{update.channel_id}")
-    if not await cleanmode_off(chat_id):
-        return
-    if chat_id not in clean:
-        clean[chat_id] = []
-    time_now = datetime.now()
-    put = {
-        "msg_id": message_id,
-        "timer_after": time_now + timedelta(minutes=AUTO_DELETE),
-    }
-    clean[chat_id].append(put)
-    await set_queries(1)
-
 
 @app.on_message(filters.command(BROADCAST_COMMAND) & SUDOERS)
 @language
