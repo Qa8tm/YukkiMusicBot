207e648a14572737512737fb15776f1a6ea6c12a
UUCCTT <98736460+UUCCTT@users.noreply.github.com>
2022-03-18 20:56:39 +0200

    Update play.py

--- YukkiMusic/utils/decorators/play.py
+++ YukkiMusic/utils/decorators/play.py
@@ -101,7 +101,8 @@ def PlayWrapper(command):
         if playty != "Everyone":
             if message.from_user.id not in SUDOERS:
                 admins = adminlist.get(message.chat.id)
-                If not admin:
+                if not admins:
+                    return await message.reply_text(_["**قم باعاده تحديث البوت بكتابة الامر** \n /Reload \n **لتحديث قائمه الادمنيه**"])
                 else:
                     if message.from_user.id not in admins:
                         return await message.reply_text(_["play_4"])
