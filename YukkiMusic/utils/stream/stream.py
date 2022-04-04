1ab360d63d793e3ba7caa47a2a883927b4b94e7f
UUCCTT <98736460+UUCCTT@users.noreply.github.com>
2022-04-04 22:01:08 +0200

    Update stream.py

--- YukkiMusic/utils/stream/stream.py
+++ YukkiMusic/utils/stream/stream.py
@@ -288,7 +288,7 @@ async def stream(
             button = telegram_markup(_)
             await elnqyb.reply_photo(
                 photo=f"https://telegra.ph//file/87df80418564dd50949e8.jpg",
-                caption=f"**File Add To Playlist**» {position}\n\n**Song Name** : {title[:30]}\n**Duration Time** : {duration_min}\n**Request By** :{requester}",
+                caption=f"**File Add To Playlist** » {position}\n\n**Song Name** : {title[:30]}\n**Duration Time** : {duration_min}\n**Request By** :{requester}",
                 reply_markup=InlineKeyboardMarkup(button),
             )
         else:
@@ -311,10 +311,11 @@ async def stream(
             )
             if video:
                 await add_active_video_chat(chat_id)
+            requester = f"[{user_name}](tg://user?id={user_id})"
             button = telegram_markup(_)
             await elnqyb.reply_photo(
                 photo=f"https://telegra.ph//file/87df80418564dd50949e8.jpg",
-                caption=f"**Starting Stream Playing**\n\n**Song Name** : {title[:30]}\n**Duration Time** : {duration_min}\n**Request By** :{requester}",
+                caption=f"**Start Playing**\n\n**Song Name** : {title[:30]}\n**Duration Time** : {duration_min}\n**Request By** :{requester}",
                 reply_markup=InlineKeyboardMarkup(button),
             )
     elif streamtype == "live":
