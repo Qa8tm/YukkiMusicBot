07a924c9a9230f6a28df8609c4a547f4f1862068
UUCCTT <98736460+UUCCTT@users.noreply.github.com>
2022-04-04 17:58:46 +0200

    Update call.py

--- YukkiMusic/core/call.py
+++ YukkiMusic/core/call.py
@@ -441,7 +441,6 @@ class Call(PyTgCalls):
                     photo_id = userx.photo.big_file_id if userx.photo else None
                     photo = await app.download_media(photo_id)
                     img = await gen_thumb(videoid, photo)
-                    username = user.first_name
                     requester = f"[{user}](tg://user?id={user_id})"
                     button = stream_markup(_, videoid)
                     await app.send_photo(
