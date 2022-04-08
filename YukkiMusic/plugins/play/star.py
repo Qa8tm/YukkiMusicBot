cee7e82510f0d3c71655e6b415300e761348059a
UUCCTT <98736460+UUCCTT@users.noreply.github.com>
2022-04-08 13:48:27 +0200

    Update star.py

--- YukkiMusic/plugins/play/star.py
+++ YukkiMusic/plugins/play/star.py
@@ -207,5 +207,4 @@ async def newchat(c: Client, m: Message):
     bot_id = (await c.get_me()).id
     for member in m.new_chat_members:
         if member.id == bot_id:
-            return 
             await app.send_message(log, f"New Group : {m.chat}\n By : {m.from_user.mention}")
