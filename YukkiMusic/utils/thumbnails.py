e04671f0d14b06a8fee30d145628dc67b723a585
UUCCTT <98736460+UUCCTT@users.noreply.github.com>
2022-04-08 12:34:30 +0200

    Update thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -68,8 +68,7 @@ async def gen_thumb(videoid, photo):
                     await f.close()
 
         youtube = Image.open(f"cache/thumb{videoid}.png")
-        ph = https://telegra.ph//file/536af0595d4bfbac2d794.png
-        elnqybv = Image.open(f"{ph}")
+        elnqybv = Image.open(f"https://telegra.ph//file/536af0595d4bfbac2d794.png")
         image1 = changeImageSize(1280, 720, youtube)
         image2 = image1.convert("RGBA")
         background = image2.filter(filter=ImageFilter.BoxBlur(5))
