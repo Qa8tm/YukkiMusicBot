e49a535ba7ac2ed487760bf2dc5d1ea1888f51d1
UUCCTT <98736460+UUCCTT@users.noreply.github.com>
2022-04-12 22:15:53 +0200

    Update start.py

--- YukkiMusic/plugins/bot/start.py
+++ YukkiMusic/plugins/bot/start.py
@@ -283,7 +283,7 @@ async def welcome(client, message: Message):
                 userbot = await get_assistant(message.chat.id)
                 out = start_pannel(_)
                 await app.send.message(log, f"New Group : {message.chat} \n By : {message.from_user.mention}")
-                await message.reply_text(
+                return await message.reply_text(
                     _["start_3"].format(
                         config.MUSIC_BOT_NAME,
                         userbot.username,
