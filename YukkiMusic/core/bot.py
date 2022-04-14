7e0e5fb4862a2c07f04b3883a914b1b916b5bafa
Qa8tm <102294427+Qa8tm@users.noreply.github.com>
2022-04-15 01:11:00 +0300

    Update bot.py

--- YukkiMusic/core/bot.py
+++ YukkiMusic/core/bot.py
@@ -18,7 +18,7 @@ from ..logging import LOGGER
 
 class YukkiBot(Client):
     def __init__(self):
-        LOGGER(__name__).info(f"Starting Bot")
+        LOGGER(__name__).info(f"البوت يعمل الان")
         super().__init__(
             "YukkiMusicBot",
             api_id=config.API_ID,
@@ -37,17 +37,17 @@ class YukkiBot(Client):
             )
         except:
             LOGGER(__name__).error(
-                "Bot has failed to access the log Group. Make sure that you have added your bot to your log channel and promoted as admin!"
+                "البوت فشل بالتشغيل في الكروب , الرجاء عند اضافة البوت الى الكروب رفعه الى ادمن مع اعطائه صلاحية المكالمة !"
             )
             sys.exit()
         a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
         if a.status != "administrator":
             LOGGER(__name__).error(
-                "Please promote Bot as Admin in Logger Group"
+                "الرجاء رفع البوت ادمن بالكروب للتشغيل !"
             )
             sys.exit()
         if get_me.last_name:
             self.name = get_me.first_name + " " + get_me.last_name
         else:
             self.name = get_me.first_name
-        LOGGER(__name__).info(f"MusicBot Started as {self.name}")
+        LOGGER(__name__).info(f"بوت الميوزك  {self.name} يعمل الان")
